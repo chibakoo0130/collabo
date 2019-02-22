3bf308ff23634f479128760aeba29391b854a95d
kotarocb <kohtarocb@gmail.com>
2019-02-22 10:55:47 +0000

    create top.html

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9399bf745a8ea2f9f1867be3fb8f10ca3c274e6efe68230a8638ce0ff5ea9a2c15b1f3280c3728c17611375bee40c58bcd00b7c9bf6f81877a1cf73847a2deec'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+  root 'homes#top'
+  get 'articles/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
+
+  post '/articles' => 'articles#create'
+
+  get '/articles' => 'articles#index'
+
+  get '/articles/:id' => 'articles#show', as: 'article'
+
+  # 更新
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+
+  # 削除
+  delete '/articles/:id' => 'articles#destroy', as: 'destroy_article'
 
 end
